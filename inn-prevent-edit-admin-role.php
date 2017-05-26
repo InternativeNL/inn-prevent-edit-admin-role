c5506f9756597c8027ad458ec559875003215a63
Roy van der Loo <royvanderloo@MacBook-Pro-van-Roy.local>
2017-05-26 10:40:20 +0200

    Basis versie klaargezet

---
+++ inn-prevent-edit-admin-role.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Internative prevent edit admin role
+Plugin URI: www.internative.nl
+Description: Prevent other roles then admins from deleting, editing, or creating an administrator
+Author: Internative
+Version: 0.0.1
+Author URI: www.internative.nl
+License: GPLv2 or later
+Text Domain: inn-prevent-edit-admin-role
+GitHub Plugin URI: InternativeNL/inn-prevent-edit-admin-role
+*/
+
+require_once ( dirname( __FILE__ ) . '/lib/admin.php' );
